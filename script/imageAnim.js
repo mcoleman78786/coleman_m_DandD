c749c9bc2a2fc73dbe64f6f7d524339bc3b26fbf
Mallory Coleman <mallorycoleman@Mallorys-MacBook-Air.local>
2019-03-04 22:14:01 -0500

    fixed one debugger

--- script/imageAnim.js
+++ script/imageAnim.js
@@ -2,24 +2,31 @@
 	//variables always come first
 	// set up the puzzle pieces and boards
 	const pieces = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
+	// hook up javascript
 
 	let piecesBoard = document.querySelector(".puzzle-pieces"),
 		puzzleBoard = document.querySelector(".puzzle-board"),
 		puzzleSelectors = document.querySelectorAll("#buttonHolder img");
+		// link up images and puzzle pieces
 
 		let dropZones = document.querySelectorAll('.drop-zone');
 
 		//functions go in the middle
 	function createPuzzlePieces(pictureIndex) {
-		//generate puzzle pieces for left hand side
+		// Always check by using debugger;
+		// link up images for puzzle 
+		// 
 		//debugger;
 		pieces.forEach((piece, index) => {
 			let newPuzzlePiece =`<img draggable id ="piece${index}" class="puzzle-image" src="images/${piece + pictureIndex}.jpg" alt="thumbnail">`;
 
 			piecesBoard.innerHTML += newPuzzlePiece;
+			// debugger;
 		});
 
 		puzzleBoard.style.backgroundImage = `url(images/backGround${pictureIndex}.jpg)`;
+		// insert image background 
+		// debugger;
 
 		initDrag();
 
@@ -29,23 +36,22 @@
 	function initDrag() {
 		piecesBoard.querySelectorAll('img').forEach(img => {
 			img.addEventListener("dragstart", function(e) {
-				//e.preventDefault(); 
 				console.log('dragging it')
 
 				e.dataTransfer.setData("text/plain", this.id);
+	    });
 	});
-	});
-	}
+}
 
 	dropZones.forEach(zone => {
 		zone.addEventListener("dragover", function(e) {
 			e.preventDefault();
-			console.log("you dragged me over");
+			console.log("dragged pieces over");
 		});
 
 		zone.addEventListener("drop", function(e) {
 			e.preventDefault();
-			console.log("you dropped");
+			console.log("dropped");
 			let firstpuzDrop = e.target;
 				while (firstpuzDrop !== 0 && !firstpuzDrop.classList.contains("drop-zone")) {
 				firstpuzDrop = firstpuzDrop.parentNode;
@@ -63,19 +69,23 @@
 	function resetPuzzlePieces() {
 		// swap out all the images when clicking on bottom button
 		//debugger;
-		//empty thumbnail container
 		piecesBoard.innerHTML = "";
+
 		createPuzzlePieces(this.dataset.puzzleref); 
 		var images = document.getElementsByClassName("puzzle-image");
-    		while(images.length > 4){
-        	images[4].parentNode.removeChild(images[4]);
-	    }
-	}
 
-	// event handling down here
+    		while(images.length > 4){
+    			
+        	images[4].parentNode.removeChild(images[4]);
+    }
+}
+
+	
 	puzzleSelectors.forEach(puzzle => puzzle.addEventListener("click", resetPuzzlePieces));
+	// handling goes down here
 
 
 
 	createPuzzlePieces(0);
+
 })();
