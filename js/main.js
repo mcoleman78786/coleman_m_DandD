523e8fbcfeac3408c531b74616d7a53d19fadeda
Mallory Coleman <mallorycoleman@Mallorys-MacBook-Air.local>
2019-03-04 21:53:11 -0500

    updated debuggers

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 (() => {
 	// select the elements that you want to work with
 	let theButton = document.querySelector("#buttonHolder img");
-	window.addEventListener("load", changeHeaderText);
+	//swindow.addEventListener("load", changeHeaderText);
 
 	function changeHeaderText() {
-		document.querySelector("h1"),textContent = "hello from javascript"
+		document.querySelector("h1").textContent = "hello from javascript"
 	}
 
 // Event handling goes at the bottom
